3f62ba94a405068c698451075293c71d3c57fd9e
rtspw <richard.cx.tang@gmail.com>
2022-08-18 19:25:17 -0700

    Add catch-all middleware at bottom to redirect to home

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@ import favicon from 'koa-favicon'
 import serve from 'koa-static'
 import helmet from 'koa-helmet'
 import morgan from 'koa-morgan'
+import path from 'path'
 
 import responseTimeHeader from './middleware/response-time-header'
 
 import router from './routers/main-router'
-import path from 'path'
 
 const app = new Koa()
 
+app.use(responseTimeHeader({ logTime: true }))
 app.use(helmet())
 app.use(morgan('combined'))
-app.use(responseTimeHeader({ logTime: true }))
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(router.routes())
 app.use(serve('web/build'))
+app.use(ctx => ctx.redirect('/'))
 
 app.listen(process.env.PORT)
